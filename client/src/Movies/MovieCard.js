24966bf17e8e50b96c2b5191ff81954c8c6f351c
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-26 13:41:10 -0500

    refactor MovieList to use MovieCard

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = props => {
-  return;
+const MovieCard = ({movie: { title, director, metascore, stars, id  }}) => {
+  return (
+    <Link to={`/movies/${id}`} className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </Link>
+  );
 };
 
 export default MovieCard;
